b51dee0228314a7c2462231c24bb3ebf5fd27cfa
qql2 <2482076557@qq.com>
2024-10-02 10:00:49 +0800

    feat: find duplicate key

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prefer-const */
-import { Modal, Notice, Plugin, TFile, parseYaml } from 'obsidian';
+import { Modal, Notice, Plugin, TFile, parseYaml } from "obsidian";
 
-import { YAML } from "qql1-yaml"
-import { parseDocument } from 'yaml';
+import { YAML } from "qql1-yaml";
+import { parse, parseDocument } from "yaml";
 
 // Remember to rename these classes and interfaces!
 
@@ -11,8 +11,8 @@ interface Settings {
 }
 
 const DEFAULT_SETTINGS: Settings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class YAML_CHECKER extends Plugin {
 	settings: Settings;
@@ -21,89 +21,94 @@ export default class YAML_CHECKER extends Plugin {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'check all YAML format',
-			name: 'Check the YAML format in all the markdowns',
+			id: "check all YAML format",
+			name: "Check the YAML format in all the markdowns",
 			callback: async () => {
 				let filesWithError: { file: TFile; error: Error }[] = [];
 
 				for (let file of this.app.vault.getMarkdownFiles()) {
 					try {
-						if (await this.hasEmptyField(file)) {
-							let error = new Error('Empty field in YAML');
-							filesWithError.push({ file, error })
-						}
+						await this.parseYAML(file);
 					} catch (error) {
 						filesWithError.push({ file, error });
 					}
 				}
 
 				if (filesWithError.length > 0) {
-					new Notice(`Found ${filesWithError.length} markdown files with invalid YAML format`);
+					new Notice(
+						`Found ${filesWithError.length} markdown files with invalid YAML format`
+					);
 					console.log(filesWithError);
 					this.showError(filesWithError);
 				} else {
-					new Notice('All markdown files have valid YAML format');
+					new Notice("All markdown files have valid YAML format");
 				}
-			}
+			},
 		});
 	}
-	async hasEmptyField(file: TFile) {
-		let yaml = await this.GetYAMLtxt(file, this)
-		if (!yaml) return false;
-		return YAML.hasEmptyValue(yaml);
-	}
-	showError(filesWithError: { file: TFile; error: Error; }[]) {
+	showError(filesWithError: { file: TFile; error: Error }[]) {
 		let errorList = new Modal(this.app);
 		errorList.onOpen = () => {
 			let { contentEl } = errorList;
-			contentEl.createEl('h2', { text: 'Invalid YAML format' });
-			let list = contentEl.createEl('ul');
+			contentEl.createEl("h2", { text: "Invalid YAML format" });
+			let list = contentEl.createEl("ul");
 			for (let { file, error } of filesWithError) {
-				let listItem = list.createEl('li');
-				listItem.createEl('a', { text: file.path, attr: { href: `obsidian://open?vault=${encodeURIComponent(file.vault.getName())}&file=${encodeURIComponent(file.path)}` } });
-				listItem.createEl('p', { text: error.message });
+				let listItem = list.createEl("li");
+				listItem.createEl("a", {
+					text: file.path,
+					attr: {
+						href: `obsidian://open?vault=${encodeURIComponent(
+							file.vault.getName()
+						)}&file=${encodeURIComponent(file.path)}`,
+					},
+				});
+				listItem.createEl("p", { text: error.message });
 			}
 		};
 		errorList.open();
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 
 	async parseYAML(file: TFile): Promise<any> {
-		let yaml = await this.GetYAMLtxt(file, this)
+		let yaml = await this.GetYAMLtxt(file, this);
 		if (!yaml) return null;
-		let doc = parseDocument(yaml)
-		doc.toString()
+		parse(yaml);
 	}
 	async getYAMLPos(file: TFile, plugin: Plugin) {
 		const doc = await plugin.app.vault.read(file);
-		const hasYAMLRgx = /(^---\n)(.)*?(\n?---(\n|$))/gs
+		const hasYAMLRgx = /(^---\n)(.)*?(\n?---(\n|$))/gs;
 		let hasYAML = false;
-		let str = '';
+		let str = "";
 		let index = 4;
 		doc.replace(hasYAMLRgx, (word, ...arg) => {
 			hasYAML = true;
 			if (word) str = word;
 			index = arg.slice(-2)[0];
 			return word;
-		})
+		});
 		const FinalLineOffset = str.length - 1 + index;
 		if (!hasYAML) return null;
 		return { start: 4, end: FinalLineOffset - 4 };
 	}
 	async GetYAMLtxt(file: TFile, plugin: Plugin) {
 		let yamlPos = await this.getYAMLPos(file, plugin);
-		if (!yamlPos || yamlPos.end - yamlPos.start == -1) return ''
-		const txt = (await plugin.app.vault.read(file)).slice(yamlPos.start, yamlPos.end + 1)
+		if (!yamlPos || yamlPos.end - yamlPos.start == -1) return "";
+		const txt = (await plugin.app.vault.read(file)).slice(
+			yamlPos.start,
+			yamlPos.end + 1
+		);
 		return txt;
 	}
 }
